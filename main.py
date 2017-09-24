2a7e7327954d67bb4f637700b4f00585a439b281
Kazumi Ikema <k.ikema@gmail.com>
2017-09-24 09:04:45 +0900

    テストイベントで渡されたインスタンスの上大雨を切り替えるところまで

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
 import boto3
+import logging
 
-REGION = "ap-northeast-1"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 ec2_client = boto3.client("ec2")
 
 def handle(event,context):
 
-    instance_ids = event["instanceids"]
+    if event.has_key("instanceids"):
+        instance_ids = event["instanceids"]
+    else:
+        instance_ids = [
+        ]
+    
+    if len(instance_ids) <= 0:
+        return "Target is empty."
+
     for instance_id in instance_ids:
         ec2_instance_state = ec2_client.describe_instances(
-            Filters = [
+            Filters=[
                 {
                     'Name': 'instance-id',
                     'Values': [
@@ -20,5 +31,21 @@ def handle(event,context):
         )["Reservations"][0]["Instances"][0]["State"]["Name"]
         print instance_id + " : " + ec2_instance_state
 
+        if ec2_instance_state == "stopped":
+            response = start_instance(instance_id)
+        else:
+            response = stop_instance(instance_id)
+
+        current_state = response["CurrentState"]["Name"]
+        logger.info("Instance %s status was turned into %s." % (instance_id, current_state))
+
+def start_instance(instance_id):
+    response = ec2_client.start_instances(InstanceIds=[instance_id])
+    return response["StartingInstances"][0]
+
+def stop_instance(instance_id):
+    response = ec2_client.stop_instances(InstanceIds=[instance_id])
+    return response["StoppingInstances"][0]
+
 if __name__ == "__main__":
-    handle({},{})
+    handle({}, {})
